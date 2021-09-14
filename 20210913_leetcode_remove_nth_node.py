9133f6c9178f12c3ee18d5a11f335d438b7ef873
Jong Shin <jshin.m@gmail.com>
2021-09-14 02:25:38 -0400

    recursion practice

--- 20210913_leetcode_remove_nth_node.py
+++ 20210913_leetcode_remove_nth_node.py
@@ -1,2 +1,20 @@
 # Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
+#recursion practice
+def sum(n):
+    if n != 0:
+        return n + sum(n-1)
+
+    return 0
+
+print(sum(4)) #returns 10
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    return fibonacci(n-1) + fibonacci(n-2) #nth number is n-1th number + n-2th number, both of which can be found recursively
+
+print(fibonacci(5)) #0 1 1 2 3
