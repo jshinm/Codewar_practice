48ef676f77da54c06181abace3e4bb355211de5b
Jong Shin <jshin.m@gmail.com>
2021-09-17 21:54:30 -0400

    key:value solution for valid parenthesis

--- 20210916_leetcode_valid_parentheses.py
+++ 20210916_leetcode_valid_parentheses.py
@@ -4,3 +4,47 @@
 
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
+
+# Example 1:
+
+# Input: s = "()"
+# Output: true
+# Example 2:
+
+# Input: s = "()[]{}"
+# Output: true
+# Example 3:
+
+# Input: s = "(]"
+# Output: false
+# Example 4:
+
+# Input: s = "([)]"
+# Output: false
+# Example 5:
+
+# Input: s = "{[]}"
+# Output: true
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        dicts = {'{':'}', '[':']', '(':')'}
+        lst = []
+
+        for char in s:
+            if char in dicts:
+                lst.append(char)
+            elif char in dicts.values() and lst != []:
+                temp = [key for key, value in dicts.items() if value == char][0]
+                if temp == lst[-1]:
+                    lst.pop()
+                else:
+                    return False
+            else: #edge case when only closed
+                return False
+
+        if lst != []: #edge case when pair not found
+            return False
+
+        return True
