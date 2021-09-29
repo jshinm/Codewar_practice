e9a7d53846a257a6a0d5aed83b7e113146bfceda
Jong Shin <jshin.m@gmail.com>
2021-09-28 23:16:40 -0400

    add binary search method

--- 20210928_leetcode_search_in_rotated_array.py
+++ 20210928_leetcode_search_in_rotated_array.py
@@ -35,7 +35,7 @@
 from typing import List
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search_I(self, nums: List[int], target: int) -> int:
         #pythonic simple solution
         try:
             return nums.index(target)
@@ -43,6 +43,50 @@ class Solution:
             print(e)
             return -1
 
+    def search_II(self, nums: List[int], target: int) -> int:
+        #binary search method
+        #3 pointers: l, m, r where m is (l+r)/2
+        #between l & m: 
+        #   there exists target => bring r to m otherwise bring l to m
+        #between m & r:
+        #   there exists target => bring l to m otherwise bring r to m
+
+        if nums == []:
+            return nums
+
+        if target not in nums: #edge case
+            return -1
+
+        l = 0
+        r = len(nums)-1
+
+        while l <= r:
+
+            m = int((l+r) / 2)
+
+            if nums[m] == target: #if target found, return m
+                return m
+
+            if nums[l] <= nums[m]: 
+                #if left search field is sorted
+
+                if nums[l] <= target <= nums[m]: #if target exists
+                    r = m
+                else:
+                    l = m + 1 #immediately outside of the search parameter
+            else: 
+                #outside of the sorted left search field
+                #there exists three conditions:
+                #1. unsorted left search field (ie rotated)
+                #2. sorted right search field
+                #3. unsorted right search field
+                if nums[m] <= target <= nums[r]: #if target exists
+                    l = m
+                else:
+                    r = m - 1 #immediately outside of the search parameter
+
+        return -1
+
 sol = Solution()
-val = sol.search([1,3,5,2,6,7], 4)
+val = sol.search_I([1,3,5,2,6,7], 4)
 print(val)
