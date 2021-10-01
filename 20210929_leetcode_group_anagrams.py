18c79a0ea886a2845371f351775aca4d59fcd538
Jong Shin <jshin.m@gmail.com>
2021-10-01 00:37:31 -0400

    brute force solution

--- 20210929_leetcode_group_anagrams.py
+++ 20210929_leetcode_group_anagrams.py
@@ -28,4 +28,32 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-      pass
+        #brute force solution
+        #take each item sort to check their equality
+        #then add to a new nested list
+        #original list is popped
+        #TC: n^2, worst case
+        
+        ans = []
+
+        while strs != []:
+            
+            temp_i = strs.pop()
+            temp_lst = [temp_i]
+
+            temp_i = ''.join(sorted(temp_i))
+
+            for i in range(len(strs)-1, -1, -1): 
+                #(n, 0) => [n-1, -1]
+                #going reverse will allow correct book keeping of the index
+
+                if len(strs[i]) != len(temp_i):
+                    continue
+
+                if ''.join(sorted(strs[i])) == temp_i:
+                    temp_lst.append(strs.pop(i))
+
+            ans.append(temp_lst)
+
+        return ans
+
