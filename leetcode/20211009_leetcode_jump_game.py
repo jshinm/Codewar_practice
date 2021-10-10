9421124e5724a2b9c7bcfae2b0741dc57cb8a3d9
Jong Shin <jshin.m@gmail.com>
2021-10-09 22:00:19 -0400

    response to modified question

--- leetcode/20211009_leetcode_jump_game.py
+++ leetcode/20211009_leetcode_jump_game.py
@@ -23,4 +23,31 @@
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        pass
+        #if the question were to indicate the consecutive jumps
+        #while loop {run until reach the end OR reach zero that is not the last index}
+        #[1,1,1,2] => True
+        #[1,10,1] => True
+        
+        if len(nums) == 1: #single item is always true
+            return True
+        
+        idx = 0
+        
+        try:            
+            while True:
+                idx += nums[idx] #move to next index
+                
+                if idx >= len(nums)-1: #if the next passes the last idx, then it's true
+                    return True
+
+                if idx + nums[idx] >= len(nums)-1: 
+                    #if the number pointed by the next index is greater than 
+                    #or equal to the size of the array, it's true
+                    return True                     
+
+                if nums[idx] == 0 and idx != len(nums)-1: 
+                    #the only false case is if there's zero prior to reaching the last index
+                    return False
+                
+        except Exception as e:
+            print(e)
