e682af4270599de806f856787f8470ff341607a4
Jong Shin <jshin.m@gmail.com>
2021-10-15 22:08:26 -0400

    sort list + find overlap conditions

--- leetcode/20211015_leetcode_merge_intervals.py
+++ leetcode/20211015_leetcode_merge_intervals.py
@@ -22,4 +22,30 @@
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        pass
+        
+        #if end_i >= start_i+1, then they overlaps
+        #but must be sorted => O(nlog(n))
+        #TC: O(N)
+        
+        ans = []
+        
+        if len(intervals) == 1:
+            return intervals
+        
+        intervals.sort()
+        
+        s_i, e_i = intervals.pop(0)
+        
+        for int_i in intervals:
+            temp_s, temp_e = int_i
+            
+            if e_i >= temp_s:
+                s_i = [s_i,temp_s][s_i >= temp_s] #whichever starts smaller
+                e_i = [e_i,temp_e][e_i <= temp_e] #whichever ends larger
+            else:
+                ans.append([s_i, e_i])
+                s_i, e_i = temp_s, temp_e
+                
+        ans.append([s_i, e_i])
+                
+        return ans
