3b2fb70821f3be425d7c7297bcfddc461513424a
Jong Shin <jshin.m@gmail.com>
2021-10-25 20:42:58 -0400

    weighted mean

--- hacker_rank/20211025_hackerrank_statistics_day0.py
+++ hacker_rank/20211025_hackerrank_statistics_day0.py
@@ -34,3 +34,42 @@ for i in out:
 
 # the output list is already sorted, so sorted by freq once more will be enough
 print(sorted(dic.items(), key=lambda x:x[1], reverse=True)[0][0]) #mode
+
+
+# Objective
+# In the previous challenge, we calculated a mean. In this challenge, we practice calculating a weighted mean. 
+# Check out the Tutorial tab for learning materials and an instructional video!
+
+# Task
+# Given an array, X, of N integers and an array, W, representing 
+# the respective weights of X's elements, calculate and print the weighted mean of X's elements.
+# Your answer should be rounded to a scale of 1 decimal place.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
+
+def weightedMean(X, W):
+    out = [i * j for i, j in zip (X, W)]
+    print (sum(out) / sum(W))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+
+    weights = list(map(int, input().rstrip().split()))
+
+    weightedMean(vals, weights)
