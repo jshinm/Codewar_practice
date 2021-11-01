f7164774f7e62ed7ef4dbc5b3e21c13c4a3190b7
Jong Shin <jshin.m@gmail.com>
2021-11-01 01:45:35 -0400

    IQR implementation

--- hacker_rank/20211101_hackerrank_statistics_day1_IQR.py
+++ hacker_rank/20211101_hackerrank_statistics_day1_IQR.py
@@ -27,9 +27,45 @@ import sys
 #  2. INTEGER_ARRAY freqs
 #
 
+def median(arr, N):
+    
+    if N % 2 == 0:
+        idx = [int(N/2-1), int(N/2)]
+    elif N % 2 == 1:
+        idx = [int(N/2)]
+
+    num = [arr[i] for i in idx]
+    
+    return sum(num)/len(num), idx
+    
+
+def quartiles(arr):
+    # Write your code here
+    arr = sorted(arr)
+    Q2, idx = median(arr, len(arr))
+    
+    if len(idx) == 1:
+        Q1, _ = median(arr[:idx[0]], len(arr[:idx[0]]))
+        Q3, _ = median(arr[idx[0]+1:], len(arr[idx[0]+1:]))
+    else:
+        Q1, _ = median(arr[:idx[0]+1], len(arr[:idx[0]+1]))
+        Q3, _ = median(arr[idx[1]:], len(arr[idx[1]:]))
+
+    return [Q1,Q2,Q3]
+
 def interQuartile(values, freqs):
     # Print your answer to 1 decimal place within this function
-    pass
+    # expand values according to freqs
+    # compute IQR based on expanded values
+
+    all_val = []
+
+    for i, n in enumerate(freqs):
+        all_val += [values[i]]*n
+
+    quart = quartiles(all_val)
+
+    print (round(quart[2]-quart[0],1))
 
 if __name__ == '__main__':
     n = int(input().strip())
