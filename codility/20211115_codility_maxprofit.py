bdccf938df0ae26bd8a2775f32c3e953c6acb861
Jong Shin <jshin.m@gmail.com>
2021-11-15 16:56:09 -0500

    min(), max() solution

--- codility/20211115_codility_maxprofit.py
+++ codility/20211115_codility_maxprofit.py
@@ -96,3 +96,15 @@ def solution(A):
         return 0
     else:
         return out
+
+# https://www.martinkysel.com/codility-maxprofit-solution/
+def solution(A):
+    max_profit = 0
+    max_day = 0
+    min_day = 200000
+     
+    for day in A:
+        min_day = min(min_day, day)
+        max_profit = max(max_profit, day-min_day)
+     
+    return max_profit
