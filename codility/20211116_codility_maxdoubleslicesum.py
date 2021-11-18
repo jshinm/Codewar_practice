f6072c890f4acf1d5df1516065e9162014c9f7d6
Jong Shin <jshin.m@gmail.com>
2021-11-18 04:42:29 -0500

    3 way single pass check

--- codility/20211116_codility_maxdoubleslicesum.py
+++ codility/20211116_codility_maxdoubleslicesum.py
@@ -46,4 +46,92 @@
 
 def solution(A):
     # write your code in Python 3.6
-    pass
+    # find the best sequence [i:j] up to N-2th index
+    # N-2 for divider, N-1 for the item, N as not counted according to the statement
+    # find the best subsequent sequence [j+1:N-1]
+    
+    # but the divider must be considered in the process
+    # such that two of either left or right side to the divider should be chosen
+    # after which best sum is chosen
+    # left first then right => A[1:-2] then A[maxi+2:-1]
+    # right first then left => A[2:-1] then A[1:maxi]
+    # compute the max of these two scenarios
+
+    # alternative case involves getting the max sequence from A[1:-1]
+    # take out the min val as a divider
+
+    # alternatively take only the positive number on the left and max on the right
+    # this doesn't work with simple [1,2,3,4,5]
+
+    # the following only scores 50%
+    
+    out = []
+
+    if len(A) < 4:
+        return 0
+
+    tmax = cmax = A[1]
+    maxarr = carr = [A[1]]
+
+    for n in A[2:-1]:
+        if cmax+n >= n:
+            cmax += n
+            carr.append(n)
+        else:
+            cmax = n
+            carr = []
+
+        if cmax > tmax:
+            tmax = cmax
+            maxarr = carr.copy()
+
+    out = [sum(maxarr) - min(maxarr)]
+
+    tmax = cmax = A[1]
+    maxi = 1
+
+    for i, n in enumerate(A[2:-2]):
+        cmax = max(n+cmax, n)
+        if tmax != max(cmax, tmax):
+            tmax = max(cmax, tmax)
+            maxi = i+2
+
+    if not A[maxi+2:-1]:
+        vmax = 0
+    else:
+        cmax = A[maxi+2]
+        vmax = A[maxi+2]
+
+        for m in A[maxi+3:-1]:
+            cmax = max(m+cmax, m)
+            vmax = max(cmax, vmax)
+
+    out.append(tmax + vmax)
+
+    tmax = cmax = A[2]
+    maxi = tmpi = 2
+
+    for i, n in enumerate(A[3:-1]):
+        if n+cmax >= n:
+            cmax = n+cmax
+        else:
+            cmax = n
+            tmpi = i+3
+
+        if tmax != max(cmax, tmax):
+            tmax = max(cmax, tmax)
+            maxi = tmpi
+
+    if not A[1:maxi-1]:
+        vmax = 0
+    else:
+        cmax = A[1]
+        vmax = A[1]
+
+        for m in A[2:maxi-1]:
+            cmax = max(m+cmax, m)
+            vmax = max(cmax, vmax)
+
+    out.append(tmax + vmax)
+
+    return max(out)
