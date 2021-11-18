8a649c3d2702956cf8adce536eae93b4d1d89971
Jong Shin <jshin.m@gmail.com>
2021-11-18 07:20:14 -0500

    init

---
+++ hacker_rank/20211118_hackerrank_statistics_day4_binomial_dist.py
@@ -0,0 +1,19 @@
+# Objective
+# In this challenge, we learn about binomial distributions. Check out the Tutorial tab for learning materials!
+
+# Task
+# The ratio of boys to girls for babies born in Russia is 1.09:1. 
+# If there is 1 child born per birth, what proportion of Russian families with exactly 6 children will have at least 3 boys?
+
+# Write a program to compute the answer using the above parameters. 
+# Then print your result, rounded to a scale of 3 decimal places (i.e., 1.234 format).
+
+# Input Format
+# A single line containing the following values:
+# 1.09 1
+
+# If you do not wish to read this information from stdin, you can hard-code it into your program.
+
+# Output Format
+# Print a single line denoting the answer, rounded to a scale of  decimal places (i.e.,  format).
+
