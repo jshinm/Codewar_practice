244b476cbfacea775c996424d0c23cf65d4e28cd
Jong Shin <jshin.m@gmail.com>
2021-11-18 07:22:01 -0500

    binom function implementation

--- hacker_rank/20211118_hackerrank_statistics_day4_binomial_dist.py
+++ hacker_rank/20211118_hackerrank_statistics_day4_binomial_dist.py
@@ -17,3 +17,34 @@
 # Output Format
 # Print a single line denoting the answer, rounded to a scale of  decimal places (i.e.,  format).
 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+import math
+
+# prob of having boys is 1.09/2.09, and for girls 1.00/2.09
+# at least 3 boys is P(3 boys) + P(4 boys) + P(5 boys) + P(6 boys)
+# alternatively, 1-P(of not having 3 boys)
+# 1-(P(4 girls)+P(5 girls)+P(6 girls))
+# then it boils down to calculating the cumulative probability of binomial distribution
+# b(x,n,p) = n choose x * p^x * q^(n-x)
+
+param = []
+
+for line in sys.stdin:
+    tmp = line.replace('\n','')
+    param.append(list(map(float, tmp.split(' '))))
+
+def binom(x,n,p):
+    return math.comb(n,x) * p**x * (1-p)**(n-x)
+    
+def prob(b,g):
+    p = b/(b+g)    
+    c = 0
+    
+    for x in range(3,7):
+        c += binom(x,6,p)
+    
+    return round(c,3)
+    
+for par in param:
+    print(prob(par[0], par[1]))
