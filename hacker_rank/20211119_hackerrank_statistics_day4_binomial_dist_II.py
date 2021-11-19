c4f22b19f2c559a08d0c7bc8a423fac2a850bd2a
Jong Shin <jshin.m@gmail.com>
2021-11-19 09:58:33 -0500

    init

---
+++ hacker_rank/20211119_hackerrank_statistics_day4_binomial_dist_II.py
@@ -0,0 +1,21 @@
+# Objective
+# In this challenge, we go further with binomial distributions. 
+
+# Task
+# A manufacturer of metal pistons finds that, on average, 12% of the pistons 
+# they manufacture are rejected because they are incorrectly sized. 
+# What is the probability that a batch of 10 pistons will contain:
+
+# No more than 2 rejects?
+# At least 2 rejects?
+
+# Input Format
+# A single line containing the following values (denoting the respective 
+# percentage of defective pistons and the size of the current batch of pistons):
+
+# Output Format
+# Print the answer to each question on its own line:
+# The first line should contain the probability that a batch of  pistons will contain no more than  rejects.
+# The second line should contain the probability that a batch of  pistons will contain at least  rejects.
+# Round both of your answers to a scale of 3 decimal places (i.e., 1.234 format).
+
