6c735dc7ac66afa09fd935a69974e675206f6d03
Jong Shin <jshin.m@gmail.com>
2021-11-19 10:43:47 -0500

    init

---
+++ hacker_rank/20211119_hackerrank_statistics_day4_geometric_dist_II.py
@@ -0,0 +1,14 @@
+# Objective
+# In this challenge, we go further with geometric distributions.
+
+# Task
+# The probability that a machine produces a defective product is 1/3. 
+# What is the probability that the 1st defect is found during the first 5 inspections?
+
+# Input Format
+# The first line contains the respective space-separated numerator and denominator 
+# for the probability of a defect, and the second line contains the inspection 
+# we want the probability of the first defect being discovered by:
+
+# Output Format
+# Print a single line denoting the answer, rounded to a scale of 3 decimal places (i.e., 1.234 format).
