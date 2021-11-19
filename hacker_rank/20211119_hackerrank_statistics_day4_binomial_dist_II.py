e16b3e461c7985ffd6bbf6cc39bae9bb7e2ca74f
Jong Shin <jshin.m@gmail.com>
2021-11-19 09:59:11 -0500

    binom solution

--- hacker_rank/20211119_hackerrank_statistics_day4_binomial_dist_II.py
+++ hacker_rank/20211119_hackerrank_statistics_day4_binomial_dist_II.py
@@ -19,3 +19,38 @@
 # The second line should contain the probability that a batch of  pistons will contain at least  rejects.
 # Round both of your answers to a scale of 3 decimal places (i.e., 1.234 format).
 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+import math
+
+# no more than 2 rejects: P(no reject) + P(1 reject) + P(2 rejects)
+# at least 2 rejects: 1 - [P(no reject) + P(1 reject)]
+
+param = []
+
+for line in sys.stdin:
+    tmp = line.replace('\n','')
+    param.append(list(map(float, tmp.split(' '))))
+
+def binom(x,n,p):
+    return math.comb(n,x) * p**x * (1-p)**(n-x)
+
+def prob(b,g):
+    p = 0.12 #prob of reject
+    c = 0 #cumulative prob
+    
+    #no more than 2 rejects
+    for x in range(0,3):
+        c += binom(x,10,p)
+    
+    print(round(c,3))
+    
+    c = 0 #reset value 
+    #at least 2 rejects
+    for x in range(0,2):
+        c += binom(x,10,p)
+    
+    print(round(1 - c, 3))
+    
+for par in param:
+    prob(par[0], par[1])
