ba4f51c61e30566a4ea978d42ed216c0185a4b8c
Jong Shin <jshin.m@gmail.com>
2021-11-19 10:31:45 -0500

    basic geometric dist

--- hacker_rank/20211119_hackerrank_statistics_day4_geometric_dist_I.py
+++ hacker_rank/20211119_hackerrank_statistics_day4_geometric_dist_I.py
@@ -12,3 +12,22 @@
 
 # Output Format
 # Print a single line denoting the answer, rounded to a scale of 3 decimal places (i.e., 1.234 format).
+
+# prob of defective product: 1/3, p=1/3
+# prob of normal product: 2/3, q=2/3
+# 1st p at the 5th => geometric distribution
+# g(n,p) = q^(n-1)*p = 1/3^(5-1)*(2/3)
+
+import sys
+
+param = []
+
+for line in sys.stdin:
+    tmp = line.replace('\n', '')
+    param += list(map(float,tmp.split(' ')))
+
+def geo(n,p):
+    q = 1-p
+    return round(q**(n-1)*p,3)
+
+print(geo(n=param[2],p=param[0]/param[1]))
