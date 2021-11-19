1bece8b58d595aba2ff49648b2f1b095f1e62dfe
Jong Shin <jshin.m@gmail.com>
2021-11-19 10:45:39 -0500

    cumulative geometric dist

--- hacker_rank/20211119_hackerrank_statistics_day4_geometric_dist_II.py
+++ hacker_rank/20211119_hackerrank_statistics_day4_geometric_dist_II.py
@@ -12,3 +12,25 @@
 
 # Output Format
 # Print a single line denoting the answer, rounded to a scale of 3 decimal places (i.e., 1.234 format).
+
+# first defect found DURING the first 5 inspections
+# p(within 5 trials) = p(1st) + p(2nd) + p(3rd) + p(4th) + p(5th)
+
+import sys
+
+param = []
+
+for line in sys.stdin:
+    tmp = line.replace('\n', '')
+    param += list(map(float,tmp.split(' ')))
+
+def geo(n,p):
+    q = 1-p
+    prob = 0
+    
+    for i in range(1,int(n+1)):
+        prob += q**(i-1)*p
+        
+    return round(prob,3)
+
+print(geo(n=param[2],p=param[0]/param[1]))
