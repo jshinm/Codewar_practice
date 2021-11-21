b8b0f9819ffde7e97b80679945f79ade4b6aacc0
Jong Shin <jshin.m@gmail.com>
2021-11-21 07:56:19 -0500

    gauss and its cdf implementation

--- hacker_rank/20211120_hackerrank_statistics_day5_normal_dist_I.py
+++ hacker_rank/20211120_hackerrank_statistics_day5_normal_dist_I.py
@@ -19,3 +19,31 @@
 
 # On the first line, print the answer to question 1 (i.e., the probability that a car can be assembled in less than 19.5 hours).
 # On the second line, print the answer to question 2 (i.e., the probability that a car can be assembled in between 20 to 22 hours).
+
+# normal equation N(u,s) = 1/(s*sqrt(2pi))*e^-((x-u)^2/(2s^2))
+# cdf = 1/2*(1+erf((x-u)/(s*sqrt(2))))
+
+import sys
+import math
+from math import sqrt, pi, exp, erf
+
+param = []
+
+for line in sys.stdin:
+    tmp = line.replace('\n','')
+    param.append(list(map(float,tmp.split(' '))))
+    
+def gauss(u,s,x):
+    out = 1/(s*sqrt(2*pi)) * exp(-(x-u)**2/(2*s**2))
+    
+    return out
+
+def cdf(u,s,x):
+    out = 0.5 * (1 + erf((x-u)/(s*sqrt(2))))
+    
+    return out
+    
+u, s = param[0][0], param[0][1]
+
+print(round(cdf(u,s, 19.5),3))
+print(round(cdf(u,s, param[2][1]) - cdf(u,s, param[2][0]),3))
