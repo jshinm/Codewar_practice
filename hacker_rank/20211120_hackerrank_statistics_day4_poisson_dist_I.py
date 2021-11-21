379a94d2a1b62ee1ce6b7745a248d448d9265702
Jong Shin <jshin.m@gmail.com>
2021-11-20 23:12:38 -0500

    init

---
+++ hacker_rank/20211120_hackerrank_statistics_day4_poisson_dist_I.py
@@ -0,0 +1,12 @@
+# Objective
+# In this challenge, we learn about Poisson distributions.
+
+# Task
+# A random variable, X, follows Poisson distribution with mean of 2.5. 
+# Find the probability with which the random variable X is equal to 5.
+
+# Input Format
+# The first line contains 's mean. The second line contains the value we want the probability for:
+
+# Output Format
+# Print a single line denoting the answer, rounded to a scale of 3 decimal places (i.e., 1.234 format).
