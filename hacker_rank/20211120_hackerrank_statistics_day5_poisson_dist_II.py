891e0d2e4d2d30f3debcd18ab756beb8232b00b1
Jong Shin <jshin.m@gmail.com>
2021-11-21 06:18:35 -0500

    init

---
+++ hacker_rank/20211120_hackerrank_statistics_day5_poisson_dist_II.py
@@ -0,0 +1,20 @@
+# Objective
+# In this challenge, we go further with Poisson distributions.
+
+# Task
+# The manager of a industrial plant is planning to buy a machine of either type A or type B. For each day’s operation:
+
+# The number of repairs, X, that machine A needs is a Poisson random variable with mean 0.88. The daily cost of operating A is C_A=160+40X^2.
+# The number of repairs, Y, that machine B needs is a Poisson random variable with mean 1.55. The daily cost of operating B is C_B=128+40Y^2.
+# Assume that the repairs take a negligible amount of time and the machines are maintained nightly 
+# to ensure that they operate like new at the start of each day. 
+# Find and print the expected daily cost for each machine.
+
+# Input Format
+# A single line comprised of 2 space-separated values denoting the respective means for A and B:
+
+# Output Format
+# There are two lines of output. Your answers must be rounded to a scale of 3 decimal places (i.e., 1.234 format):
+
+# On the first line, print the expected daily cost of machine A.
+# On the second line, print the expected daily cost of machine B.
