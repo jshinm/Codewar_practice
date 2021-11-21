9f6ff055f1b5ce9564e8b62b5e39575cacf1f3a8
Jong Shin <jshin.m@gmail.com>
2021-11-21 08:48:32 -0500

    gaussian in range

--- hacker_rank/20211120_hackerrank_statistics_day5_normal_dist_II.py
+++ hacker_rank/20211120_hackerrank_statistics_day5_normal_dist_II.py
@@ -23,3 +23,30 @@
 # On the first line, print the answer to question 1.
 # On the second line, print the answer to question 2.
 # On the third line, print the answer to question 3.
+
+# upper limit not known thus 1-cdf(x)
+
+import sys
+import math
+from math import sqrt, pi, exp, erf
+
+param = []
+
+for line in sys.stdin:
+    tmp = line.replace('\n','')
+    param.append(list(map(float,tmp.split(' '))))
+    
+def gauss(u,s,x):
+    out = 1/(s*sqrt(2*pi)) * exp(-(x-u)**2/(2*s**2))
+    
+    return out
+
+def cdf(u,s,x):
+    out = 0.5 * (1 + erf((x-u)/(s*sqrt(2))))
+    
+    return out
+    
+u, s = param[0][0], int(param[0][1])
+print(round((1-cdf(u,s, 80))*100,2))
+print(round((1-cdf(u,s, 60))*100,2))
+print(round((cdf(u,s, 60))*100,2))
