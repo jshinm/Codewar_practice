cd436ec7fa447181298d121295bb0c7af449f1bb
Jong Shin <jshin.m@gmail.com>
2021-11-21 06:52:56 -0500

    poisson characteristics

--- hacker_rank/20211120_hackerrank_statistics_day5_poisson_dist_II.py
+++ hacker_rank/20211120_hackerrank_statistics_day5_poisson_dist_II.py
@@ -18,3 +18,32 @@
 
 # On the first line, print the expected daily cost of machine A.
 # On the second line, print the expected daily cost of machine B.
+
+# One doesn't need to actually calculate the Poisson distribution (and for that matter, we do not know k anyway). 
+# What one should do is realise that for some random Poisson variable, X, the expectation value and the variance are:
+
+# E[X] = lambda
+# and
+# var[X] = lambda
+
+# var[X] = E[X^2] + E[X]^2
+# E[X^2] = var[X] + E[X]^2
+#        = lambda + lambda^2
+
+# The question specifically asks for the average during the day (with each day being independent of each other), 
+# and hence the expectation value is that, the average of all possible events over the day.
+
+# As given, daily cost C_A is 160+40X^2, thus its expected value would be
+# 160+40*E[X^2], which is solved as above for poisson distribution
+
+import sys
+import math
+
+param = []
+
+for line in sys.stdin:
+    tmp = line.replace('\n', '')
+    param += list(map(float, tmp.split(' ')))
+
+print(round(160+40*(param[0]+param[0]**2),3))
+print(round(128+40*(param[1]+param[1]**2),3))
