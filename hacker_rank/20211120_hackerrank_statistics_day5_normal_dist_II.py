04b1568013f1fe08c886a5ce1b234662a523f52d
Jong Shin <jshin.m@gmail.com>
2021-11-21 08:10:04 -0500

    init

---
+++ hacker_rank/20211120_hackerrank_statistics_day5_normal_dist_II.py
@@ -0,0 +1,25 @@
+# Objective
+# In this challenge, we go further with normal distributions.
+
+# Task
+# The final grades for a Physics exam taken by a large group of students have a mean of u=70 and a standard deviation of s=10. 
+# If we can approximate the distribution of these grades by a normal distribution, what percentage of the students:
+
+# Scored higher than 80 (i.e., have a grade > 80)?
+# Passed the test (i.e., have a grade >= 60)?
+# Failed the test (i.e., have a grade < 80)?
+
+# Find and print the answer to each question on a new line, rounded to a scale of 2 decimal places.
+
+# Input Format
+# There are 3 lines of input (shown below):
+# The first line contains 2 space-separated values denoting the respective mean and standard deviation for the exam. 
+# The second line contains the number associated with question 1. 
+# The third line contains the pass/fail threshold number associated with questions 2 and 3.
+
+# Output Format
+# There are three lines of output. Your answers must be rounded to a scale of 3 decimal places (i.e., 1.234 format):
+
+# On the first line, print the answer to question 1.
+# On the second line, print the answer to question 2.
+# On the third line, print the answer to question 3.
