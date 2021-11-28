d17df9d9713e52abb87bb30a5ca05b71c90c5e33
Jong Shin <jshin.m@gmail.com>
2021-11-28 06:30:53 -0500

    init

---
+++ hacker_rank/20211120_hackerrank_statistics_day6_central_limit_I.py
@@ -0,0 +1,18 @@
+# Objective
+# In this challenge, we practice solving problems based on the Central Limit Theorem.
+
+# Task
+# A large elevator can transport a maximum of 9800 pounds. 
+# Suppose a load of cargo containing 49 boxes must be transported via the elevator.
+# The box weight of this type of cargo follows a distribution with a mean of 205 pounds and a standard deviation of 15 pounds. 
+# Based on this information, what is the probability that all 49 boxes can be safely loaded into the freight elevator and transported?
+
+# The first line contains the maximum weight the elevator can transport. 
+# The second line contains the number of boxes in the cargo. 
+# The third line contains the mean weight of a cargo box, 
+# and the fourth line contains its standard deviation.
+
+# Output Format
+# Print the probability that the elevator can successfully transport all 49 boxes, 
+# rounded to a scale of 4 decimal places (i.e., 1.2345 format).
+
