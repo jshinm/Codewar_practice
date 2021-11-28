3665770981660b47d6d728cbae2c371c0f7f0445
Jong Shin <jshin.m@gmail.com>
2021-11-28 06:56:14 -0500

    CLT for practical example

--- hacker_rank/20211120_hackerrank_statistics_day6_central_limit_I.py
+++ hacker_rank/20211120_hackerrank_statistics_day6_central_limit_I.py
@@ -16,3 +16,27 @@
 # Print the probability that the elevator can successfully transport all 49 boxes, 
 # rounded to a scale of 4 decimal places (i.e., 1.2345 format).
 
+# for large n, dist of sample mean approaches normal dist
+# here we have total pounds are x, and number of samples is n
+# where n=49, x=9800
+# N(u, s) where u=n*u and s=sqrt(n)*s
+
+import sys
+import math
+from math import sqrt, pi, exp, erf
+
+param = []
+
+for line in sys.stdin:
+    tmp = line.replace('\n','')
+    param += list(map(int,tmp.split(' ')))
+    
+def cdf(u,s,x):
+    out = 0.5 * (1 + erf((x-u)/(s*sqrt(2))))
+    
+    return out
+    
+n, x = param[1], param[0]
+u, s = param[2], param[3]
+
+print(round(cdf(n*u, math.sqrt(n)*s, x),4))
