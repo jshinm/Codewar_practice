947c8a80e51b5dd5a4d89c54e113865cffbc0f5f
Jong Shin <jshin.m@gmail.com>
2024-06-30 21:03:46 -0400

    Add string solution

--- leetcode/20210821_leetcode_palindrome_check.py
+++ leetcode/20210821_leetcode_palindrome_check.py
@@ -1,57 +1,67 @@
 # Given an integer x, return true if x is palindrome integer.
 
-# An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+# An integer is a palindrome when it reads the same backward
+# as forward. For example, 121 is palindrome while 123 is not.
+
 
 class Solution:
-    #string solution
+    def isPalindrome(str):
+        # String palindrome checker
+        # Run loop from 0 to len/2
+        for i in range(0, int(len(str) / 2)):
+            if str[i] != str[len(str) - i - 1]:
+                return False
+        return True
+
+    # string solution
     def isPalindrome_int(self, x: int) -> bool:
-        
-        if x >= -2**31 and x <= 2**31 - 1:
-        
+
+        if x >= -(2**31) and x <= 2**31 - 1:
             temp = str(x)
 
             if temp == temp[::-1]:
                 return True
-        
+
         return False
 
-    #integer solution - search the half
+    # integer solution - search the half
     def isPalindrome(self, x: int) -> bool:
-        
-        if x >= -2**31 and x <= 2**31 - 1:
-                        
+
+        if x >= -(2**31) and x <= 2**31 - 1:
+
             if x < 10:
                 if x < 0:
                     return False
                 else:
                     return True
-                
-            if x % 10 == 0: #the last digit can't be zero
+
+            if x % 10 == 0:  # the last digit can't be zero
                 return False
-            
+
             newSeq = 0
-            
-            while (x >= newSeq):                
+
+            while x >= newSeq:
 
                 if newSeq == x:
                     return True
-                                
+
                 newSeq *= 10
                 newSeq += x % 10
                 x = int(x / 10)
-                
+
                 if x == 0:
                     return False
-                
+
                 # print(newSeq, x)
-                
+
             if x == int(newSeq / 10):
                 return True
-            
-        return False   
+
+        return False
+
 
 # public boolean isPalindrome(int num){
-#    if(num < 0) return  false; 
+#    if(num < 0) return  false;
 #    int reversed = 0, remainder, original = num;
 #    while(num != 0) {
 #         remainder = num % 10; // reversed integer is stored in variable
